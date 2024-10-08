f602d0bcc7fb1133f8dae35fe674411d68f9fbea
Lucas Cheng <lucaswcheng8@gmail.com>
2024-10-07 21:26:29 -0700

    fafd

--- app/page.tsx
+++ app/page.tsx
@@ -223,7 +223,7 @@ export default function SecretPage() {
     <div className={styles.body}>
       {/* FONTS */}
       <link href="https://fonts.cdnfonts.com/css/jumbo-sale-trial" rel="stylesheet"></link>
-      <style>@import url('https://fonts.cdnfonts.com/css/jumbo-sale-trial');</style>
+      {/* <style>@import url('https://fonts.cdnfonts.com/css/jumbo-sale-trial');</style> */}
       {/* BACKROUND */}
       <img className={styles.backround} src="toiletPaperBackround.png" alt="" />
       {/* LEFT MENU */}
