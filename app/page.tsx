5e55bbcab3cca2586075e3df483e3e11a8f44267
Lucas Cheng <lucaswcheng8@gmail.com>
2024-10-12 15:23:13 -0700

    ben the great

--- app/page.tsx
+++ app/page.tsx
@@ -148,6 +148,12 @@ export default function SecretPage() {
     }
   }, [gameName]); 
 
+  useEffect(() => {
+    if (gameName == 'ben the great') {
+      alert('I see you are ben... I shall grant you stuf heheheh! but not until u tell me wut you need in the chat')
+    }
+  }, [gameName]); 
+
   // STOCK MARKET
     // Stock market modal toggle state
     const [isStockMarketOpen, setIsStockMarketOpen] = useState(false);
