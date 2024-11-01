a38cb35d125490a5ced96a441c04ba934fbc58d6
Lucas Cheng <lucaswcheng8@gmail.com>
2024-11-01 09:27:24 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -58,15 +58,15 @@ export default function SecretPage() {
  const initialDidChooseLanguage = JSON.parse(localStorage.getItem("didChooseLanguage") || "false")
  const [didChooseLanguage, setDidChooseLanguage] = useState(initialDidChooseLanguage);
 
- const savedLanguage = (localStorage.getItem("language") || '')
+ const savedLanguage = (localStorage.getItem("language") || 'hi')
  const [language, setLanguage] = useState(savedLanguage)
 
  // Update localStorage when `didChooseLanguage` changes
-//  useEffect(() => {
-//    if (typeof window !== 'undefined') {
-//      localStorage.setItem('didChooseLanguage', JSON.stringify(didChooseLanguage));
-//    }
-//  }, [didChooseLanguage]);
+ useEffect(() => {
+   if (typeof window !== 'undefined') {
+     localStorage.setItem('didChooseLanguage', JSON.stringify(didChooseLanguage));
+   }
+ }, [didChooseLanguage]);
 
 
  // Language selection functions
